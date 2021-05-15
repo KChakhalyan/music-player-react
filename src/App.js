54459ecc227f33bd5da58978b9222ff291f76133
Karen Chkahalyan <karenchakhalyan23@gmail.com>
2021-05-15 23:33:32 +0400

    add some data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlayerComponent from './components/PlayerComponent';
 import SongComponent from './components/SongComponent';
+import './styles/app.scss';
+import data from './util';
 
 function App() {
+	const [songs, setSongs] = useState(data());
+	const [currentSong, setCurrentSong] = useState(songs[1]);
 	return (
 		<div className="App">
-			<SongComponent />
+			<SongComponent currentSong={currentSong} />
 			<PlayerComponent />
 		</div>
 	);
--- src/components/PlayerComponent.js
+++ src/components/PlayerComponent.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 function PlayerComponent() {
 	return (
-		<div>
+		<div className="player">
 			<div className="time-control">
 				<p>Start time</p>
 				<input type="range" name="" id="" />
 				<p>End Time</p>
 			</div>
-			<div className="play-control"></div>
+			<div className="play-control">
+				<FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+				<FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+				<FontAwesomeIcon className="skip-forward" icon={faAngleRight} size="2x" />
+			</div>
 		</div>
 	);
 }
