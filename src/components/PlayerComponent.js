ca324a3a81db442c6f9d95b22fa664ef7189923c
Karen Chkahalyan <karenchakhalyan23@gmail.com>
2021-05-15 21:25:11 +0400

    add some dependencies

---
+++ src/components/PlayerComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PlayerComponent() {
+	return (
+		<div>
+			<div className="time-control">
+				<p>Start time</p>
+				<input type="range" name="" id="" />
+				<p>End Time</p>
+			</div>
+			<div className="play-control"></div>
+		</div>
+	);
+}
+
+export default PlayerComponent;
